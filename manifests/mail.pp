1918c98e12fefec2be75b59bfaf973c32c633075
Connor O <connor607@gmail.com>
2017-11-30 06:27:01 -0600

    Make mail deliverable to outside hosts

--- manifests/mail.pp
+++ manifests/mail.pp
@@ -1,10 +1,10 @@
 class tilde::mail {
 
   class {'postfix':
-    inet_interfaces => 'localhost',
+    inet_interfaces => '0.0.0.0',
   }
 
-  postfix::config { 'default_transport':
-    value => 'error: outside mail not deliverable'
-  }
+  #postfix::config { 'default_transport':
+  #  value => 'error: outside mail not deliverable'
+  #}
 }
